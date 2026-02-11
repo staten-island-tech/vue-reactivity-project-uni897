52779a6f11a052d576005038e8d54b1b17ddd818
uni897 <akhasmeer7@gmail.com>
2026-02-11 14:01:48 -0500

    fsdfsdfsdf

--- app/src/views/Title.vue
+++ app/src/views/Title.vue
@@ -1,13 +1,18 @@
 <template>
+  <h1>Duck Racing</h1>
   <div>
-    <img class="duck" src="/duck.svg" />
+    <button>Play</button>
+    <button>Customize Player 1</button>
+    <button>Customize Player 2</button>
   </div>
 </template>
 
 <script setup></script>
 
 <style scoped>
-
+h1{
+  text-align: center;
+}
 .duck{
     width: 10%;
     height: auto;
