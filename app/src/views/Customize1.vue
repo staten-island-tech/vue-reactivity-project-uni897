a68dffa9f859dc5cb8ae8c1986540f26e1569921
uni897 <akhasmeer7@gmail.com>
2026-02-13 14:00:38 -0500

    xvcxvxcv

--- app/src/views/Customize1.vue
+++ app/src/views/Customize1.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script setup>
-
+const colors = [
+    {name: "Red", color: "#ff0000"},
+    {name: "Green", color: "#008000"},
+    {name: "Blue", color: "#0000ff"},
+    {name: "Yellow", color: "#ff0000"},//default color
+    {name: "Lime", color: "#bfff00"},
+    {name: "Purple", color: "#800080"},
+    {name: "Indigo", color: "#6f00fe"},
+    {name: "Salmon", color: "#fa8072"},
+    {name: "Orange", color: "#ffa500"},
+    {name: "Sky Blue", color: "#87cefa"},
+]
 </script>
 
 <style scoped>
