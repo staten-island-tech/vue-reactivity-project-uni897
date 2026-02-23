36c6324e5d39ddcd0d36b4d6b5db8a6f0887d2a4
uni897 <akhasmeer7@gmail.com>
2026-02-23 17:16:02 -0500

    ljhvlhvjv

--- app/src/views/Customize2.vue
+++ app/src/views/Customize2.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-
+        <button> <RouterLink to="/">Back</RouterLink> </button>
+        <Duck width="100px" height="100px" class="p2" :skincolor="players[1].color"></Duck>
+        <choice v-for="color in colors" :key="color.name" :item="color" @select="updateStats(2, color.color)"/>
     </div>
 </template>
 
 <script setup>
+import choice from "@/components/choice.vue"
+import Duck from "@/components/duck.vue"
+import { players, updateStats } from "@/state/players.js"
 
+const colors = [
+    {name: "Default", color: "#FFE67B"},
+    {name: "Red", color: "#ff0000"},
+    {name: "Green", color: "#008000"},
+    {name: "Blue", color: "#0000ff"},
+    {name: "Lime", color: "#bfff00"},
+    {name: "Purple", color: "#800080"},
+    {name: "Indigo", color: "#6f00fe"},
+    {name: "Salmon", color: "#fa8072"},
+    {name: "Orange", color: "#ffa500"},
+    {name: "Sky Blue", color: "#87cefa"},
+]
 </script>
 
 <style scoped>
--- app/src/views/Play.vue
+++ app/src/views/Play.vue
@@ -1,11 +1,55 @@
 <template>
     <div>
-
+        <button> <RouterLink to="/">Back</RouterLink> </button>
+        <Duck :pos="pos1" width="100px" height="100px" class="p1" :skincolor="players[0].color"></Duck>
+        <Duck :pos="pos2" width="100px" height="100px" class="p2" :skincolor="players[1].color"></Duck>
+        <h2 v-if="pos1 > 1200">Player 1 wins!</h2>
+        <h2 v-if="pos2 > 1200">Player 2 wins!</h2>
     </div>
 </template>
 
 <script setup>
+import { players } from '@/state/players.js'
+import Duck from '@/components/duck.vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
+const pos1 = ref(0)
+const pos2 = ref(0)
+let d = false
+let arr = false
+
+const step = 50
+
+function keyPress(e) {
+  if(e.key === 'd' && !d){
+    pos1.value += step
+    d = true
+    console.log(d)
+  }
+  if(e.key === 'ArrowRight' && !arr){
+    pos2.value += step
+    arr = true
+    console.log(arr)
+  }
+}
+
+function keyUp(e){
+    if(e.key === 'd' && !(pos1.value > 1200 || pos2.value > 1200)){
+        d = false
+    }
+    if(e.key === 'ArrowRight' && !(pos1.value > 1200 || pos2.value > 1200)){
+        arr = false
+    }
+}
+
+onMounted(() => {
+    window.addEventListener('keydown', keyPress)
+    window.addEventListener('keyup', keyUp)
+})
+onUnmounted(() => {
+    window.removeEventListener('keydown', keyPress)
+    window.removeEventListener('keyup', keyUp)
+})
 </script>
 
 <style scoped>
