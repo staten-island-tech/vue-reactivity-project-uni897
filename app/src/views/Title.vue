8c96d24c61c11014b06cca4230c3936fd05223b3
uni897 <akhasmeer7@gmail.com>
2026-03-03 14:02:20 -0500

    owenismybeta

--- app/src/views/Title.vue
+++ app/src/views/Title.vue
@@ -1,21 +1,30 @@
-<template>
+<template class="github">
   <h1>Duck Racing</h1>
   <div>
-    <button><RouterLink to="/race">Play</RouterLink></button>
-    <button><RouterLink to="/player1">Player 1</RouterLink></button>
-    <button><RouterLink to="/player2">Player 2</RouterLink></button>
-    <Duck width="100px" height="100px" class="p1" :skincolor="players[0].color"></Duck>
-    <Duck width="100px" height="100px" class="p2" :skincolor="players[1].color"></Duck>
+    <RouterLink class="route" to="/race">Play</RouterLink>
+    <RouterLink class="route" to="/player1">Player 1</RouterLink>
+    <RouterLink class="route" to="/player2">Player 2</RouterLink>
   </div>
 </template>
 
-<script setup>
-import Duck from '@/components/duck.vue';
-import { players } from "@/state/players.js"
-</script>
+<script setup> </script>
 
 <style scoped>
 h1{
   text-align: center;
 }
+div{
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.route{
+  background-color: lightpink;
+  padding: 10px;
+  border-radius: 25px;
+  border: 5px solid black;
+}
+
 </style>
